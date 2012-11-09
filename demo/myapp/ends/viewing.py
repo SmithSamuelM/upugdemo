4e3d115e7a3dafdf47f4fff9ea630755df068f9c
Samuel M Smith <smith.samuel.m@gmail.com>
2012-11-08 17:48:48 -0700

    put and delete

--- demo/myapp/ends/viewing.py
+++ demo/myapp/ends/viewing.py
@@ -56,3 +56,42 @@ def teamPlayerGet(tname, name):
     if not player:
         bottle.abort(404, "Player '%s' on team '%s' not found." % (name, tname))    
     return player
+
+@app.route('/backend/team/<tname>/player/<name>/put')
+@app.route('/backend/team/<tname>/player/<name>', method='PUT') 
+def teamPlayerPut(tname, name):
+    data = bottle.request.json
+    if not data:
+        bottle.abort(400, "Empty order data in PUT body.")
+        
+    team = teaming.teams.get(tname, None)
+    if not team:
+        bottle.abort(404, "Team '%s' not found." % tname)
+    
+    player = team['players'].get(name, None)
+    if not player: #new player
+        player = teaming.DEFAULT_PLAYER
+        team['players'] = player
+    
+    player.update(data)
+    
+    return player
+
+@app.route('/backend/team/<tname>/player/<name>/del')
+@app.route('/backend/team/<tname>/player/<name>', method='DELETE') 
+def teamPlayerDelete(tname, name):
+    data = bottle.request.json
+    if not data:
+        bottle.abort(400, "Empty order data in PUT body.")
+        
+    team = teaming.teams.get(tname, None)
+    if not team:
+        bottle.abort(404, "Team '%s' not found." % tname)
+    
+    player = team['players'].get(name, None)
+    if not player: #new player
+        bottle.abort(404, "Player '%s' on team '%s' not found." % (name, tname))  
+    
+    del team['players'][name]
+    
+    return {}
